ccb54ff8ff78fe06983b83b76dd81b077a56ebf2
Jonas W. <67288183+Eskolo@users.noreply.github.com>
2024-07-02 18:13:41 +0200

    inital commit

---
+++ api/index.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, Response, jsonify
+import pyphen
+import json
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+@app.route('/hyphenation', methods=['POST'])
+def hyphenation():
+    data = request.get_json()
+    print(data)
+    text = data.get('text', '')
+    dic = pyphen.Pyphen(lang='de_DE')
+    words = text.split()
+    sylables = []
+    for word in words:
+        sylable = dic.inserted(word)
+        sylables.append(sylable)
+
+    data = {'sylables': sylables}
+    json_string = json.dumps(data,ensure_ascii = False)
+    #creating a Response object to set the content type and the encoding
+    response = Response(json_string,content_type="application/json; charset=utf-8" )
+    return response
+
+@app.route('/arsch', methods=['POST'])
+def arsch():
+    return "Arsch!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
