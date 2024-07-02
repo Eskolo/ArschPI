6c192157d1904a0b4984674b82f80996c8005dd1
Jonas W. <67288183+Eskolo@users.noreply.github.com>
2024-07-02 22:09:47 +0200

    added arschcounter

--- api/index.py
+++ api/index.py
@@ -6,6 +6,7 @@ from flask_cors import cross_origin
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+
 @app.route('/v1/hyphenation', methods=['POST'])
 @cross_origin()
 def hyphenation():
@@ -18,12 +19,25 @@ def hyphenation():
     for word in words:
         sylable = dic.inserted(word)
         sylables.append(sylable)
-        
+
+    # read the int from arschfragen.txt, incremnt by 1 and write it back
+    with open('./api/arschfragen.txt', 'r') as file:
+        arschfragen = int(file.read())
+        arschfragen += 1
+        with open('./api/arschfragen.txt', 'w') as file:
+            file.write(str(arschfragen))
+
     return jsonify({'sylables': sylables})
 
-@app.route('/arsch', methods=['POST'])
+
+@app.route('/arsch', methods=['GET'])
 def arsch():
-    return "Arsch!"
+    # read the int from arschfragen.txt
+    with open('./api/arschfragen.txt', 'r') as file:
+        # read all text from arschfragen.txt
+        arschfragen = file.read()
+        return arschfragen + " mal arschgefragt"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
