8ef1402b34abf84a238965261d024438708cdc14
Jonas W. <67288183+Eskolo@users.noreply.github.com>
2024-07-02 19:39:17 +0200

    added get method

--- api/index.py
+++ api/index.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, Response, jsonify
 import pyphen
 import json
-from flask_cors import CORS, cross_origin
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/hyphenation', methods=['POST'])
 @cross_origin()
@@ -26,6 +24,25 @@ def hyphenation():
     #creating a Response object to set the content type and the encoding
     response = Response(json_string,content_type="application/json; charset=utf-8" )
     # add corse header
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route('/get-hyphenation', methods=['GET'])
+def hyphenation():
+    text = request.args.get('text', '')
+    print(text)
+    dic = pyphen.Pyphen(lang='de_DE')
+    words = text.split()
+    sylables = []
+    for word in words:
+        sylable = dic.inserted(word)
+        sylables.append(sylable)
+
+    data = {'sylables': sylables}
+    json_string = json.dumps(data, ensure_ascii=False)
+    # Creating a Response object to set the content type and the encoding
+    response = Response(json_string, content_type="application/json; charset=utf-8")
+    # Add CORS header
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
